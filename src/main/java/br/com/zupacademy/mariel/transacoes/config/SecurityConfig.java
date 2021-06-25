d7cd6c4597dfc9ac4be6fa71a5c390b6eabc1e51
Mariel Couto <coutomariel@gmail.com>
2021-06-25 11:19:39 -0300

    Implementação da camada de segurança

---
+++ src/main/java/br/com/zupacademy/mariel/transacoes/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package br.com.zupacademy.mariel.transacoes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
